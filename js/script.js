583685eeec35527ce943647dae264eba96a88b48
Valerie <37264241+valerier2000@users.noreply.github.com>
2020-11-04 19:38:16 +0100

    timer added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var myfunc = setInterval(function(){
+    var countDownDate = new Date("Nov 16, 2020, 13:50:00").getTime();
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+    document.getElementById("days").innerHTML = days + "d"
+    document.getElementById("hours").innerHTML = hours + "h"
+    document.getElementById("mins").innerHTML = minutes + "m"
+    document.getElementById("secs").innerHTML = seconds + "s"
+}, 1000);
